201407c61de85c9936bad0db74f9d744565225f2
wnatti <wnatti@gmail.com>
2014-06-26 10:33:11 +0300

    Update PopUp.h
    jees

--- Host_client/Client/PopUp.h
+++ Host_client/Client/PopUp.h
@@ -2,6 +2,8 @@
 #include "SFML\Graphics.hpp"
 //#include <iostream>
 #include <string>
+#include "Explosion.h"
+#include <vector>
 class PopUp
 {
 public:
@@ -9,10 +11,14 @@ public:
 	bool draw(sf::RenderWindow &window);
 	~PopUp(void);
 private:
+	std::vector<Explosion>_explosions;
 	sf::RectangleShape _shape;
 	sf::Text _text;
 	sf::Font *_font;
 	sf::Clock _timer;
+	sf::Clock _timer2;
 	bool wantsToDie;
+	int trigger;
+	float delay;
 	int _time;
 };
