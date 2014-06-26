8fd0ff0ece8ba509906b38d35a5e38e89fed6b45
wnatti <wnatti@gmail.com>
2014-06-26 10:32:41 +0300

    jee
    kivasti rivissä räjähtelee

--- Host_client/Client/PopUp.cpp
+++ Host_client/Client/PopUp.cpp
@@ -9,7 +9,7 @@ PopUp::PopUp(sf::Font font,std::string message,sf::Vector2f position,sf::Vector2
 	*_font = font;
 	_text.setFont(*_font);
 	_text.setString(message);
-	
+	trigger = 0;
 	
 	_shape.setSize(size);
 	_text.setCharacterSize(size.y*0.5f);
@@ -23,21 +23,51 @@ PopUp::PopUp(sf::Font font,std::string message,sf::Vector2f position,sf::Vector2
 	float ratioy = _shape.getGlobalBounds().height/_text.getGlobalBounds().height;
 	ratioy*=0.5f;
 	_text.setScale(ratiox,ratioy);
+	delay = 0.15f;
+
+	_explosions.push_back(Explosion(sf::Vector2f(_shape.getPosition().x+_shape.getSize().x*0.5f,_shape.getPosition().y+_shape.getSize().y*0.5f)));
 	
 }
 
 bool PopUp::draw(sf::RenderWindow &window)
 {
-	if(_timer.getElapsedTime().asSeconds()>_time)
+
+	if(_timer2.getElapsedTime().asSeconds()>delay && trigger == 0)
+	{
+		_explosions.push_back(Explosion(sf::Vector2f(_shape.getPosition().x+_shape.getSize().x,_shape.getPosition().y+_shape.getSize().y*0.5f)));
+		trigger = 1;
+		_timer2.restart();
+	}
+	if(_timer2.getElapsedTime().asSeconds()>delay && trigger == 1)
+	{
+		_explosions.push_back(Explosion(sf::Vector2f(_shape.getPosition().x,_shape.getPosition().y+_shape.getSize().y*0.5f)));
+	_timer2.restart();
+	trigger = 3;
+	}
+	if(_timer2.getElapsedTime().asSeconds()>delay && trigger == 3)
+	{
+
+	}
+
+
+	if(_timer.getElapsedTime().asSeconds()>_time && time != 0)
 	{
 		wantsToDie = true;
 		return wantsToDie;
 	}
-	
+
+	for(int i = 0; i < _explosions.size();i++)
+	{
+		if(_explosions[i].update())
+			_explosions[i].draw(window);
+		else
+			_explosions.erase(_explosions.begin()+i);
+	}
+
 	window.draw(_shape);
 	window.draw(_text);
-	return wantsToDie;
 
+	return wantsToDie;
 	
 }
 
