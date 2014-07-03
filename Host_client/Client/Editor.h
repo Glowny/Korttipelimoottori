ac1baa99a62aa7977932d4526e8852df54f23567
wnatti <wnatti@gmail.com>
2014-07-03 15:36:15 +0300

    apua
    ei apua ei

--- Host_client/Client/Editor.h
+++ Host_client/Client/Editor.h
@@ -2,6 +2,10 @@
 #include "SFML\Graphics.hpp"
 #include "DropDown.h"
 #include <iostream>
+#include "ButtonObject.h"
+#include "Explosion.h"
+#include "MouseExplosion.h"
+#include "CardObject.h"
 class Editor
 {
 public:
@@ -12,8 +16,20 @@ public:
 	void initialize();
 	void checkMouseHover(sf::Vector2i mousepos);
 	void checkMouseClick(sf::Vector2i mousepos);
+	void newRule();
+	void scrollDown(int amount);
+	void scrollUp(int amount);
+	sf::Vector2f getViewOffset();
+	
 private:
+	sf::Vector2f _viewOffset;
+	int rivit;
 	sf::RenderWindow _window;
 	std::vector<DropDown>_dropDowns;
 	sf::Font *_font;
+	void arrangeDropDowns();
+	ButtonObject plusButton,minusButton;
+	std::vector<ButtonObject>_minuses;
+	sf::Text *_minus,*_plus;
 };
+
