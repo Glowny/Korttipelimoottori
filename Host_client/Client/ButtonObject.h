b76ded4f35774071c241efe2e37ff11c017034eb
wnatti <wnatti@gmail.com>
2014-07-03 15:38:09 +0300

    lol
    xd

--- Host_client/Client/ButtonObject.h
+++ Host_client/Client/ButtonObject.h
@@ -1,22 +1,22 @@
 #pragma once
 #include "SFML\Graphics.hpp"
 #include <string>
-#include "Explosion.h"
-#include <vector>
 class ButtonObject
 {
 public:
+	ButtonObject(){_font = new sf::Font;};
 	ButtonObject(sf::Font font,std::string string);
 	ButtonObject(sf::Font font,std::string string, sf::Texture texture);
 	~ButtonObject(void);
-
+	
 	void draw(sf::RenderWindow &window);
+	void setText(sf::Text *text){_text = *text;_text.setColor(sf::Color::Black);arrange();}
+	
 	void setPosition(sf::Vector2f position);
 	void setSize(sf::Vector2f size);
-	void splode();
 	sf::FloatRect getArea();
 private:
-	std::vector<Explosion> _splosions;
+	void arrange();
 	sf::Text _text;
 	sf::RectangleShape _shape;
 	sf::Sprite _sprite;
